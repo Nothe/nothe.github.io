1bccc1142d24488a1c0745ceca5208ef1d281424
Nothe <v.desmet59@gmail.com>
2018-02-19 16:35:31 +0100

    2

---
+++ JS/calculator/script.js
@@ -0,0 +1,10 @@
+
+var firstNumber = prompt ("Quelle est votre premier numéro ?");
+
+var operator = prompt ("Quelle est votre opérteur ?");
+
+var secondNumber = prompt ("Quelle est votre second numéro ?");
+
+var answer = Number(firstNumber)+operator+Number(secondNumber); 
+
+alert(eval(answer));
